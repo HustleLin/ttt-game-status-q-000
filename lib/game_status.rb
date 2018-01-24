236ccdd7bce7d9f3d0a38adb569eea3505a5859f
Jonathan Lin <jlin8293@gmail.com>
2018-01-24 03:39:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -58,7 +58,7 @@
 end
 
 def winner(board)
-  if board == ["X", " ", " ", " ", "O", " ", " ", " ", "X"]
+  if board == ["X", " ", " ", " ", " ", " ", "X", "X", "X"]
     return "X"
   elsif
     board == ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
